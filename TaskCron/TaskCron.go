95454105e21029be6612a14be3a7b65dd68c2144
wjdsg0327 <2268035794@qq.com>
2025-11-18 09:24:17 +0800

    修改指定脚本更新

--- TaskCron/TaskCron.go
+++ TaskCron/TaskCron.go
@@ -1,6 +1,7 @@
 package TaskCron
 
 import (
+	"auto-bgi/Notice"
 	"auto-bgi/OneLong"
 	"auto-bgi/abgiSSE"
 	"auto-bgi/autoLog"
@@ -52,26 +53,26 @@ var AudioService control.Audio
 func InitTaskCron() {
 	task = make(map[string]func(string))
 	task["一条龙"] = func(data string) {
-		autoLog.Sugar.Infof("定时任务启动：一条龙-现在时间:%s 参数:%s", time.Now().Format("15:04:05"), data)
+		autoLog.Sugar.Infof("定时任务启动：一条龙-现在时间:%s 参数:[%s]", time.Now().Format("15:04:05"), data)
 		taskOneLong.OneLongTask(data)
 	}
 	task["配置组"] = func(data string) {
-		autoLog.Sugar.Infof("定时任务启动：配置组-现在时间:%s 参数:%s", time.Now().Format("15:04:05"), data)
+		autoLog.Sugar.Infof("定时任务启动：配置组-现在时间:%s 参数:[%s]", time.Now().Format("15:04:05"), data)
 		split := strings.Split(data, " ")
 		taskTask.StartGroups(split)
 	}
 	task["米游社签到"] = func(data string) {
-		autoLog.Sugar.Infof("定时任务启动：米游社签到-现在时间:%s 参数:%s", time.Now().Format("15:04:05"), data)
+		autoLog.Sugar.Infof("定时任务启动：米游社签到-现在时间:%s 参数:[%s]", time.Now().Format("15:04:05"), data)
 		taskTask.MiYouSheSign()
 	}
 	task["关闭原神和关闭bgi"] = func(data string) {
-		autoLog.Sugar.Infof("定时任务启动：关闭原神和关闭bgi-现在时间:%s 参数:%s", time.Now().Format("15:04:05"), data)
+		autoLog.Sugar.Infof("定时任务启动：关闭原神和关闭bgi-现在时间:%s 参数:[%s]", time.Now().Format("15:04:05"), data)
 		control.CloseYuanShen()
 		control.CloseSoftware()
 
 	}
 	task["备份user"] = func(data string) {
-		autoLog.Sugar.Infof("定时任务启动：备份user-现在时间:%s 参数:%s", time.Now().Format("15:04:05"), data)
+		autoLog.Sugar.Infof("定时任务启动：备份user-现在时间:%s 参数:[%s]", time.Now().Format("15:04:05"), data)
 		err4 := bgiStatus.ZipDir(config.Cfg.BetterGIAddress+"\\User\\", "Users\\User"+time.Now().Format("2006-01-02-15-04-05")+".zip", true)
 		if err4 != nil {
 			autoLog.Sugar.Errorf("备份失败: %v", err4)
@@ -80,30 +81,30 @@ func InitTaskCron() {
 		autoLog.Sugar.Info("备份成功")
 	}
 	task["定时关机"] = func(data string) {
-		autoLog.Sugar.Infof("定时任务启动：定时关机-现在时间:%s 参数:%s", time.Now().Format("15:04:05"), data)
+		autoLog.Sugar.Infof("定时任务启动：定时关机-现在时间:%s 参数:[%s]", time.Now().Format("15:04:05"), data)
 		cmd := exec.Command("shutdown", "/s", "/t", "60")
 		err := cmd.Run()
 		if err != nil {
-			autoLog.Sugar.Errorf("定时任务启动：定时关机失败:%s", err)
+			autoLog.Sugar.Errorf("定时任务启动：定时关机失败:[%s]", err)
 		} else {
-			autoLog.Sugar.Infof("定时任务启动：定时关机成功:%s", err)
+			autoLog.Sugar.Infof("定时任务启动：定时关机成功:[%s]", err)
 		}
 	}
 	task["狗粮联机上线"] = func(data string) {
-		autoLog.Sugar.Infof("定时任务启动：联系上线-现在时间:%s 参数:%s", time.Now().Format("15:04:05"), data)
+		autoLog.Sugar.Infof("定时任务启动：联系上线-现在时间:%s 参数:[%s]", time.Now().Format("15:04:05"), data)
 		abgiSSE.OnStart()
 	}
 
 	task["狗粮联机调试上线"] = func(data string) {
-		autoLog.Sugar.Infof("定时任务启动：联系上线-调试-现在时间:%s 参数:%s", time.Now().Format("15:04:05"), data)
+		autoLog.Sugar.Infof("定时任务启动：联系上线-调试-现在时间:%s 参数:[%s]", time.Now().Format("15:04:05"), data)
 		abgiSSE.OnStartDebug()
 	}
 	task["电脑静音"] = func(data string) {
-		autoLog.Sugar.Infof("定时任务启动：电脑静音-现在时间:%s 参数:%s", time.Now().Format("15:04:05"), data)
+		autoLog.Sugar.Infof("定时任务启动：电脑静音-现在时间:%s 参数:[%s]", time.Now().Format("15:04:05"), data)
 		AudioService.Mute()
 	}
 	task["脚本自动更新"] = func(data string) {
-		autoLog.Sugar.Infof("定时任务启动：脚本自动更新-现在时间:%s 参数:%s", time.Now().Format("15:04:05"), data)
+		autoLog.Sugar.Infof("定时任务启动：脚本自动更新-现在时间:%s 参数:[%s]", time.Now().Format("15:04:05"), data)
 		if err := bgiStatus.BatchUpdateScript(); err != "" {
 			autoLog.Sugar.Errorf("批量更新脚本失败: %v", err)
 		} else {
@@ -111,21 +112,18 @@ func InitTaskCron() {
 		}
 	}
 	task["指定脚本更新"] = func(data string) {
-		autoLog.Sugar.Infof("定时任务启动：指定脚本更新-现在时间:%s 参数:%s", time.Now().Format("15:04:05"), data)
+		autoLog.Sugar.Infof("定时任务启动：指定脚本更新-现在时间:%s 参数:[%s]", time.Now().Format("15:04:05"), data)
 		split := strings.Split(data, " ")
-		for i, name := range split {
-			autoLog.Sugar.Infof("第%d个更新脚本：%s", i, name)
-			_, err := bgiStatus.UpdateJs(name)
-			if err != nil {
-				autoLog.Sugar.Errorf("更新脚本失败: %v", err)
-				return
-			}
-			autoLog.Sugar.Infof("更新脚本成功")
+		js, err := bgiStatus.SpecifyUpdateJs(split)
+		if err != nil {
+			autoLog.Sugar.Errorf("指定脚本更新失败: %v", err)
 		}
+		autoLog.Sugar.Infof("指定脚本更新成功: %s", js)
+		Notice.SentText("指定脚本更新成功: " + js)
 
 	}
 	task["启动bat脚本"] = func(data string) {
-		autoLog.Sugar.Infof("定时任务启动：启动bat脚本-现在时间:%s 参数:%s", time.Now().Format("15:04:05"), data)
+		autoLog.Sugar.Infof("定时任务启动：启动bat脚本-现在时间:%s 参数:[%s]", time.Now().Format("15:04:05"), data)
 		taskTask.CallBat(data)
 	}
 
--- bgiStatus/bgiJs.go
+++ bgiStatus/bgiJs.go
@@ -48,6 +48,43 @@ func ReadMd(filePath string) string {
 
 }
 
+// 指定脚本更新
+func SpecifyUpdateJs(subDirs []string) (string, error) {
+	GitPull()
+	time.Sleep(1)
+
+	// 获取本地所有订阅脚本目录
+	scriptDir := filepath.Join(config.Cfg.BetterGIAddress, "User", "JsScript")
+
+	for _, name := range subDirs {
+
+		nowVersion, _ := GetJsNowVersion(scriptDir, name)
+		newVersion, chineseName, err := GetJsNewVersion(name)
+		if err != nil {
+			continue
+		}
+
+		if tools.CompareVersion(nowVersion, newVersion) == -1 {
+			if name == "CD-Aware-AutoGather" {
+				autoLog.Sugar.Infof("脚本更新：带CD管理的自动采集有更新，版本号是：" + newVersion + "。如需更新，请手动更新")
+				Notice.SentText("带CD管理的自动采集有更新，版本号是：" + newVersion + "。如需更新，请手动更新")
+				continue
+			}
+
+			// 开始更新
+			_, err := UpdateJs(name)
+			if err != nil {
+				autoLog.Sugar.Errorf("更新脚本失败: %v", err)
+				continue
+			}
+			autoLog.Sugar.Infof("更新脚本成功: %s", chineseName)
+			Notice.SentText(fmt.Sprintf("脚本 %s 已更新,版本号:%s @所有人", chineseName, newVersion))
+		}
+
+	}
+	return "全部更新成功", nil
+}
+
 // 批量更新脚本
 func BatchUpdateScript() string {
 	GitPull()
